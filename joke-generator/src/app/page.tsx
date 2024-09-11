25224f9db5a9b6abe18caf7e56985bd9bc9c8449
Muhammad Inam <nudmaufon@gmail.com>
2024-09-11 09:34:50 +0500

    project_7

---
+++ joke-generator/src/app/page.tsx
@@ -0,0 +1,8 @@
+import GenerateJokes from "@/components/GenerateJokes";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+ <GenerateJokes/>
+  );
+}
---
+++ joke-generator/src/components/GenerateJokes.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { useEffect, useState } from "react";
+import { Button } from "./ui/button";
+
+interface JokeType {
+  setup: string;
+  punchline: string;
+}
+
+function GenerateJokes() {
+  const [jokeSetup, setJokeSetup] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchJoke = async (): Promise<void> => {
+    try {
+      const response = await fetch(
+        "https://official-joke-api.appspot.com/random_joke"
+      );
+      const data: JokeType = await response.json();
+      const joke = `${data.setup} - ${data.punchline}`;
+      setJokeSetup(joke);
+
+      
+    } catch (error: any) {
+      console.log(error);
+      setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchJoke();
+  }, []);
+
+  return (
+    <div className="flex text-black justify-center items-center h-screen w-full bg-gradient-to-tl from-[#FF9E0D] to-[#FF7951] px-8">
+        <div className=" max-w-md space-y-8 p-8 bg-white rounded-lg">
+            <h1 className="text-3xl font-bold tracking-tighter ">Random Jokes Generator</h1>
+            <div className="p-8 bg-gray-400/10 rounded-lg font-mono ">
+                    {jokeSetup}
+            </div>
+            <div>
+                <Button className="font-bold px-4 py-3 rounded-xl bg-[#43A147] hover:bg-[#1d802a]" onClick={() => fetchJoke()}>Generate New jOke 🤪</Button>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default GenerateJokes;
