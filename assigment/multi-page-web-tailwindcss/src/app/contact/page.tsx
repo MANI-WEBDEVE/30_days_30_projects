c60a1bb98241af69a053c83b6032329b043b6cab
Muhammad Inam <nudmaufon@gmail.com>
2024-11-24 16:24:55 +0500

    contact-apply-tailwindcss

--- assigment/multi-page-web-tailwindcss/src/app/contact/page.tsx
+++ assigment/multi-page-web-tailwindcss/src/app/contact/page.tsx
@@ -1,18 +1,17 @@
 import React from "react";
-import styles from "./Contact.module.css";
 const page = () => {
   return (
     <>
-      <section className={styles.contactpage}>
-        <div className={styles.contactHeading}>
+      <section className="container flex flex-col items-center justify-center gap-10">
+        <div className="text-2xl font-bold text-center">
           <h1>Contact</h1>
         </div>
-        <div className={styles.contactForm}>
-          <input type="text"  placeholder="Enter Your Name"/>
-          <input type="email"  placeholder="Enter Your Email"/>
-          <input type="text" placeholder="Enter Your Message" />
-          <div className={styles.contactButton}>
-            <button>Submit</button>
+        <div className="flex items-center gap-10 flex-col justify-center w-full">
+          <input className="py-3 pl-11 pr-10 bg-blue-400/10 rounded-lg"  type="text"  placeholder="Enter Your Name"/>
+          <input  className="py-3 pl-11 pr-10 bg-blue-400/10 rounded-lg" type="email"  placeholder="Enter Your Email"/>
+          <input  className="py-3 pl-11 pr-10 bg-blue-400/10 rounded-lg" type="text" placeholder="Enter Your Message" />
+          <div className="w-full flex items-center justify-center">
+            <button className="py-3 px-10 bg-blue-700/70 rounded-lg">Submit</button>
           </div>
         </div>
       </section>
