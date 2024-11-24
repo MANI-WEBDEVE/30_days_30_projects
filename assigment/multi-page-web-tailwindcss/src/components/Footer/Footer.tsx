2c6c484e3fca4bda341f457d29f8bdd3e2c7ab78
Muhammad Inam <nudmaufon@gmail.com>
2024-11-24 14:13:55 +0500

    footer-apply-tailwindcss

--- assigment/multi-page-web-tailwindcss/src/components/Footer/Footer.tsx
+++ assigment/multi-page-web-tailwindcss/src/components/Footer/Footer.tsx
@@ -1,17 +1,20 @@
+"use client"
 import React from 'react'
-
-import styles from "./Footer.module.css"
 
 const Footer = () => {
   return (
     <>
-      <section className={styles.footer}>
-        <div className={styles.footerText}>
-          <h1>Design</h1>
-          <p>Developed By <span>INAM</span></p>
-        </div>
-        <div className={styles.footerText}>
-          <p>Copyright &copy; 2023</p>
+      <section className="container mx-auto px-4 py-8 md:py-12">
+        <div className="flex flex-col md:flex-row justify-between items-center space-y-6 md:space-y-0">
+          <div className="text-center md:text-left">
+            <h1 className="text-3xl md:text-4xl font-extrabold tracking-normal">Design</h1>
+            <p className="text-sm md:text-base mt-2">
+              Developed By <span className="font-bold tracking-[0.5rem] ml-2">INAM</span>
+            </p>
+          </div>
+          <div className="text-sm md:text-base tracking-[0.4rem] md:tracking-[0.6rem]">
+            <p>Copyright &copy; 2023</p>
+          </div>
         </div>
       </section>
     </>
